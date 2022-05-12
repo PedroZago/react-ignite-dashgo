d60275e897dc23a69ba5e5c7b25a4298730e97e8
Pedro Zago <pedro.camargozago@hotmail.com.br>
2022-05-12 12:10:27 -0300

    Responsive Header

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,24 @@
+import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
+    <Flex align="center">
+      {
+        showProfileData && (
+          <Box mr="4" textAlign="right">
+            <Text>Pedro Zago</Text>
+            <Text color="gray.300" fontSize="small">
+              pdecamargozago@gmail.com
+            </Text>
+          </Box>
+        )
+      }
+
+      <Avatar size="md" name="Pedro Zago" src="https://github.com/pedrozago.png" />
+    </Flex>
+  )
+}
